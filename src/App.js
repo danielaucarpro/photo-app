89dab684152700d2684f4b87777d2038fe7a92a4
danielaucarpro <76978089+danielaucarpro@users.noreply.github.com>
2021-05-21 19:39:47 -0300

    testing pushing
    save the old files in another folder, and replaced my files with Kit's project.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
 import './App.css';
 import Photos from './components/Photos';
+import FetchComments from './components/FetchComments';
 
 function App() {
   return (
+    <>
+    <Photos/>
+    <FetchComments/>
+    </>
   );
 }
 
